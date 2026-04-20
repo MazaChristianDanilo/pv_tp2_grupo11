a4432550fad0fdb373a7852718b883dba49b6c8c
Danilo Christian Maza <danchr2002@gmail.com>
2026-04-20 19:10:40 -0300

    agrego ejercicio 6

---
+++ js/ejercicio6.js
@@ -0,0 +1,10 @@
+const edades = [25, 30, 35, 40, 45, 50, 55, 60];
+let suma = 0;
+for (let i = 0; i < edades.length; i++) {
+    console.log(`Edad en la posicion ${i}: ${edades[i]}`);
+    suma += edades[i];
+}
+
+const promedio = suma / edades.length;
+console.log(`El promedio de las edades es: ${promedio.toFixed(2)}`);
+
