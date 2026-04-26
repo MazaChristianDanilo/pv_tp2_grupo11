0aca76e37b1ba19e29b3ff425b4e4df5e40a0f3c
SALIM QUINTAR <safaqui00@gmail.com>
2026-04-26 18:00:41 -0300

    desarrollo ej 4

---
+++ js/ejercicio4.js
@@ -0,0 +1,10 @@
+
+let base = 10;   // medida en cm
+let altura = 5;  // medida en cm
+let area = base * altura;
+let perimetro = 2 * (base + altura);   
+console.log("--- Resultados del Rectángulo ---");
+console.log("Base: " + base + " cm");
+console.log("Altura: " + altura + " cm");
+console.log("Área: " + area + " cm²");
+console.log("Perímetro: " + perimetro + " cm");
